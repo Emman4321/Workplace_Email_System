a5571897cad094462db56da8c83717d134cb07b2
emman4321 <emmanuelfatus@gmail.com>
2025-01-24 14:43:00 -0500

    Functional and complete until further additions.

--- emailSystem/src/App.java
+++ emailSystem/src/App.java
@@ -1,5 +1,59 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your first name: "); 
+        String firstName = scanner.nextLine();
+
+        System.out.println("Enter your last name: ");
+        String lastName = scanner.nextLine();
+
+        Email em1 = new Email(firstName, lastName);
+
+        em1.setMailboxCapacity(500);
+        em1.setAltEmail("umbcretriever@gmail.com");
+
+        String choice = "";
+        while (!choice.equals("exit")){
+            System.out.println("Enter a command: display, changepass,upgradecap, updatealt, or exit");
+            choice = scanner.nextLine();
+
+            if(choice.equals("display")){
+                System.out.println(em1.showInfo());
+            } 
+            else if (choice.equals("changepass")){
+                System.out.println("Enter your new password: ");
+                String newPassword = scanner.nextLine();
+                
+                em1.changePassword(newPassword);
+                System.out.println("Your new password is: " + newPassword);
+            }
+            else if (choice.equals("updatealt")){
+                System.out.println("Enter your new alternate email: ");
+                String newAltEmail = scanner.nextLine();
+                
+                em1.setAltEmail(newAltEmail);
+                System.out.println("Your new alternate email is: " + newAltEmail);
+            }
+            else if (choice.equals("upgradecap")){
+                System.out.println("Enter your new mailbox capacity: ");
+                int newCapacity = scanner.nextInt();
+                scanner.nextLine();
+                
+                em1.setMailboxCapacity(newCapacity);
+                System.out.println("Your new mailbox capacity is: " + newCapacity);
+            }
+            else if (choice.equals("exit")){
+                System.out.println("Goodbye!");
+            }
+            else{
+                System.out.println("Invalid input. Please try again.");
+            }
+        
+        } 
+        
+    scanner.close();
     }
 }
---
+++ emailSystem/src/Email.java
@@ -0,0 +1,86 @@
+import java.util.Scanner;
+
+public class Email {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private String email;
+    private int mailboxCapacity = 500;
+    private String altEmail;
+    private String compSuffix = "company.com";
+
+    //input first name and last name
+    public Email(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("Hello, " + this.firstName + " " + this.lastName + "!");
+
+        //call a method asking for department, then return department
+        this.department = setDepartment();
+        System.out.println("Department: " + this.department);
+
+        this.password = randPass(12);
+        System.out.println("Your current password is: " + this.password);
+
+        email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + department + ".company.com";
+        System.out.println("Your new email is: " + email);
+    }   
+
+    //ask for department
+    private String setDepartment(){
+        System.out.println("Enter the department\n1 for Sales\n2 for Tech\n3 for Marketing\n4 for Finance\n" + //
+                        "5 for Executive\n0 for none");
+        Scanner in = new Scanner(System.in);
+        int depChoice = in.nextInt();
+        if(depChoice == 1) { return "sales"; }
+        else if(depChoice == 2) { return "tech"; }
+        else if(depChoice == 3) { return "marketing"; }
+        else if(depChoice == 4) { return "finance"; }
+        else if(depChoice == 5) { return "executive"; }
+        else { return ""; }
+
+    }
+
+    //gen random password
+    private String randPass(int length){
+        length = 12;
+        String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%";
+        char[] password = new char[length];
+        for(int i = 0; i < length; i++){
+            int rand = (int)(Math.random() * passwordSet.length());
+            password[i] = passwordSet.charAt(rand);
+        }
+        return new String(password);
+    }
+    //set mailbox capacity
+    public void setMailboxCapacity(int capacity){
+        this.mailboxCapacity = capacity;
+    }   
+        //set alt email
+    public void setAltEmail(String altEmail){  
+        this.altEmail = altEmail;
+    }
+        //change password
+    public void changePassword(String password){
+        this.password = password;
+    }
+        //get mailbox capacity
+    public int getMailboxCapacity(){
+        return mailboxCapacity;
+    }
+        //get alt email
+    public String getAltEmail(){
+        return altEmail;
+    }
+        //get password
+    public String getPassword(){
+        return password;
+    }
+    //shows current user info
+    public String showInfo(){
+        return "Display Name: " + firstName + " " + lastName + 
+                "\nCompany Email: " + email + 
+                "\nMailbox Capacity: " + mailboxCapacity + "mb";
+    }
+}
